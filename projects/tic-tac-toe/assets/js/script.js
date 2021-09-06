55776dfcccdaac356936ce6c1c2bfff2e7a9a519
JM Arenas <rns.jey@gmail.com>
2021-09-06 18:31:32 +0800

    Added winning player indicator in the board

--- projects/tic-tac-toe/assets/js/script.js
+++ projects/tic-tac-toe/assets/js/script.js
@@ -75,30 +75,44 @@ function checkWinner(a,b,c) {
   return (a === b && b === c && a !== "")
 }
 
+const divMain = document.getElementById("main");
+
 function theWinner() {
+  const horizonMark = document.querySelectorAll(".horizontal");
+  const verticalMark = document.querySelectorAll(".vertical");
+  const diagonalMark = document.querySelectorAll(".diagonal");
+
   let winner = null;
 
   //Horizontal
   for (let row = 0; row < 3; row++) {
     if (checkWinner(board[row][0],board[row][1],board[row][2])) {
       winner = board[row][0];
-    } 
+      horizonMark[row].classList.add(currPlayer === players[0] ? "blue" : "red")
+      horizonMark[row].style.visibility = "visible";
+    }
   }
 
   //Vertical
   for (let col = 0; col < 3; col++) {
     if (checkWinner(board[0][col],board[1][col],board[2][col])) {
       winner = board[0][col];
+      verticalMark[col].classList.add(currPlayer === players[0] ? "blue" : "red")
+      verticalMark[col].style.visibility = "visible";
     } 
   }
 
   //Diagonal
   if (checkWinner(board[0][0],board[1][1],board[2][2])) {
     winner = board[0][0];
+    diagonalMark[0].classList.add(currPlayer === players[0] ? "blue" : "red")
+    diagonalMark[0].style.visibility = "visible";
   }
 
   if (checkWinner(board[0][2],board[1][1],board[2][0])) {
     winner = board[0][2];
+    diagonalMark[1].classList.add(currPlayer === players[0] ? "blue" : "red")
+    diagonalMark[1].style.visibility = "visible";
   }
 
   return winner;
@@ -109,8 +123,8 @@ function nextPlayer() {
   const player2 = document.getElementById("player2");
 
   currPlayer = currPlayer === players[0] ? players[1] : players[0];
-  player1.className = currPlayer === players[0] ? "current-player" : "";
-  player2.className = currPlayer === players[1] ? "current-player" : "";
+  player1.className = currPlayer === players[0] ? "current-player blue" : "";
+  player2.className = currPlayer === players[1] ? "current-player red" : "";
 }
 
 function saveMove(row,col) {
