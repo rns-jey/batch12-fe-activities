a3ecfce6e182e80f99ff3ffc223adc6fea4c0965
JM Arenas <rns.jey@gmail.com>
2021-09-08 18:43:12 +0800

    Added a better winning indicator
    changed from line indicator to change tile color

--- projects/tic-tac-toe/assets/js/script.js
+++ projects/tic-tac-toe/assets/js/script.js
@@ -34,6 +34,7 @@ let available = []
 let isGameover = false;
 let players = ["O","X"];
 let currPlayer = players[0];
+let winningTrio = [];
 
 const rowLet = ['a','b','c'];
 let myBoard = document.getElementById("ttt-board");
@@ -93,6 +94,7 @@ function theWinner() {
   for (let row = 0; row < 3; row++) {
     if (checkWinner(board[row][0],board[row][1],board[row][2])) {
       winner = board[row][0];
+      winningTrio = [`${rowLet[row]}0`,`${rowLet[row]}1`,`${rowLet[row]}2`]
     }
   }
 
@@ -100,16 +102,19 @@ function theWinner() {
   for (let col = 0; col < 3; col++) {
     if (checkWinner(board[0][col],board[1][col],board[2][col])) {
       winner = board[0][col];
+      winningTrio = [`a${col}`,`b${col}`,`c${col}`]
     } 
   }
 
   //Diagonal
   if (checkWinner(board[0][0],board[1][1],board[2][2])) {
     winner = board[0][0];
+    winningTrio = [`a0`,`b1`,`c2`]
   }
 
   if (checkWinner(board[0][2],board[1][1],board[2][0])) {
     winner = board[0][2];
+    winningTrio = [`a2`,`b1`,`c0`]
   }
 
   return (winner === "O" ? "You win!" : winner === "X" ? "You lose!" : null);
@@ -144,6 +149,7 @@ function disableBtn() {
 
   if (historyIndex === (history.length - 1)) {
     btnNext.disabled = true;
+    displayWinner()
   } else if (historyIndex < (history.length - 1)) {
     btnNext.disabled = false;
   }
@@ -181,11 +187,13 @@ async function aiTurn() {
 
 const player1 = document.getElementById("player1");
 const player2 = document.getElementById("player2");
+const p1Name = document.getElementById("p1-name");
+const p2Name = document.getElementById("p2-name");
 
 function nextPlayer() {
   currPlayer = currPlayer === players[0] ? players[1] : players[0];
-  player1.className = currPlayer === players[0] ? "current-player blue" : "";
-  player2.className = currPlayer === players[1] ? "current-player red" : "";
+  player1.className = currPlayer === players[0] ? "current-player" : "";
+  player2.className = currPlayer === players[1] ? "current-player" : "";
 
   if (currPlayer === players[1]) {
     aiTurn();
@@ -194,12 +202,22 @@ function nextPlayer() {
 
 const playerShell = document.getElementById("players");
 const gameOverShell = document.getElementById("game-over");
+const winnerName = document.getElementById("the-winner");
+
+function displayWinner(cls) {
+  for (let i = 0; i < winningTrio.length; i++) {
+    const tile = document.querySelector(`.${winningTrio[i]}`);
+    
+    tile.classList.add("winning-trio");
+    tile.classList.add(cls)
+  }
+}
 
 function gameOver() {
   isGameover = true;
   player1.className = "";
   player2.className = "";
-  historyIndex = history.length - 1;
+  historyIndex = history.length - 1; 
 
   playerShell.style.display = "none"
   gameOverShell.style.display = "block"
@@ -211,14 +229,14 @@ function saveMove(row,col) {
   saveHistory();
   
   if (result !== null) {
-    alert(result)
+    winnerName.innerHTML = currPlayer === players[0] ? `${p1Name.innerHTML} Wins!` : `${p2Name.innerHTML} Wins!`;
+    displayWinner(currPlayer === players[0] ? "blue" : "red")
     gameOver()
-    
   } else {
     if (available.length > 0) {
       nextPlayer()
     } else {
-      alert(`It's a tie!`)
+      winnerName.innerHTML = "It's a Tie!";
       gameOver()
     }
   }
